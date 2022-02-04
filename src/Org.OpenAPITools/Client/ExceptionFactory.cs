b73055e72d50c48bd0be3bf00db74dd238f746c8
jwenger100 <Jon.Wenger@gmail.com>
2022-02-04 14:28:09 -0800

    2.4.19

---
+++ src/Org.OpenAPITools/Client/ExceptionFactory.cs
@@ -0,0 +1,34 @@
+/*
+ * AvaTax Software Development Kit for C#
+ *
+ * (c) 2004-2022 Avalara, Inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Avalara Shipping Verification only
+ *
+ * API for evaluating transactions against direct-to-consumer Beverage Alcohol shipping regulations.  This API is currently in beta. 
+ *
+
+ * @author     Sachin Baijal <sachin.baijal@avalara.com>
+ * @author     Jonathan Wenger <jonathan.wenger@avalara.com>
+ * @copyright  2004-2022 Avalara, Inc.
+ * @license    https://www.apache.org/licenses/LICENSE-2.0
+ * @version    2.4.19
+ * @link       https://github.com/avadev/AvaTax-REST-V3-DotNet-SDK
+ */
+
+
+using System;
+
+namespace Org.OpenAPITools.Client
+{
+    /// <summary>
+    /// A delegate to ExceptionFactory method
+    /// </summary>
+    /// <param name="methodName">Method name</param>
+    /// <param name="response">Response</param>
+    /// <returns>Exceptions</returns>
+    public delegate Exception ExceptionFactory(string methodName, IApiResponse response);
+}
---
+++ src/Org.OpenAPITools/Client/IReadableConfiguration.cs
@@ -0,0 +1,127 @@
+/*
+ * AvaTax Software Development Kit for C#
+ *
+ * (c) 2004-2022 Avalara, Inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Avalara Shipping Verification only
+ *
+ * API for evaluating transactions against direct-to-consumer Beverage Alcohol shipping regulations.  This API is currently in beta. 
+ *
+
+ * @author     Sachin Baijal <sachin.baijal@avalara.com>
+ * @author     Jonathan Wenger <jonathan.wenger@avalara.com>
+ * @copyright  2004-2022 Avalara, Inc.
+ * @license    https://www.apache.org/licenses/LICENSE-2.0
+ * @version    2.4.19
+ * @link       https://github.com/avadev/AvaTax-REST-V3-DotNet-SDK
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Org.OpenAPITools.Client
+{
+    /// <summary>
+    /// Represents a readable-only configuration contract.
+    /// </summary>
+    public interface IReadableConfiguration
+    {
+        /// <summary>
+        /// Gets the access token.
+        /// </summary>
+        /// <value>Access token.</value>
+        string AccessToken { get; }
+
+        /// <summary>
+        /// Gets the API key.
+        /// </summary>
+        /// <value>API key.</value>
+        IDictionary<string, string> ApiKey { get; }
+
+        /// <summary>
+        /// Gets the API key prefix.
+        /// </summary>
+        /// <value>API key prefix.</value>
+        IDictionary<string, string> ApiKeyPrefix { get; }
+
+        /// <summary>
+        /// Gets the base path.
+        /// </summary>
+        /// <value>Base path.</value>
+        string BasePath { get; }
+
+        /// <summary>
+        /// Gets the date time format.
+        /// </summary>
+        /// <value>Date time format.</value>
+        string DateTimeFormat { get; }
+
+        /// <summary>
+        /// Gets the default header.
+        /// </summary>
+        /// <value>Default header.</value>
+        [Obsolete("Use DefaultHeaders instead.")]
+        IDictionary<string, string> DefaultHeader { get; }
+
+        /// <summary>
+        /// Gets the default headers.
+        /// </summary>
+        /// <value>Default headers.</value>
+        IDictionary<string, string> DefaultHeaders { get; }
+
+        /// <summary>
+        /// Gets the temp folder path.
+        /// </summary>
+        /// <value>Temp folder path.</value>
+        string TempFolderPath { get; }
+
+        /// <summary>
+        /// Gets the HTTP connection timeout (in milliseconds)
+        /// </summary>
+        /// <value>HTTP connection timeout.</value>
+        int Timeout { get; }
+
+        /// <summary>
+        /// Gets the proxy.
+        /// </summary>
+        /// <value>Proxy.</value>
+        WebProxy Proxy { get; }
+
+        /// <summary>
+        /// Gets the user agent.
+        /// </summary>
+        /// <value>User agent.</value>
+        string UserAgent { get; }
+
+        /// <summary>
+        /// Gets the username.
+        /// </summary>
+        /// <value>Username.</value>
+        string Username { get; }
+
+        /// <summary>
+        /// Gets the password.
+        /// </summary>
+        /// <value>Password.</value>
+        string Password { get; }
+
+        /// <summary>
+        /// Gets the API key with prefix.
+        /// </summary>
+        /// <param name="apiKeyIdentifier">API key identifier (authentication scheme).</param>
+        /// <returns>API key with prefix.</returns>
+        string GetApiKeyWithPrefix(string apiKeyIdentifier);
+
+        /// <summary>
+        /// Gets certificate collection to be sent with requests.
+        /// </summary>
+        /// <value>X509 Certificate collection.</value>
+        X509CertificateCollection ClientCertificates { get; }
+    }
+}
